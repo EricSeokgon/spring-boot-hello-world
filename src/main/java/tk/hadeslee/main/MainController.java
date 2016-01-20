4591426cb90946c761c004fb5442446e1308195c
hadesyi <hadesleeq@gmail.com>
2016-01-20 17:51:23 +0900

    jpa setting

--- src/main/java/tk/hadeslee/main/MainController.java
+++ src/main/java/tk/hadeslee/main/MainController.java
@@ -1,8 +1,13 @@
 package tk.hadeslee.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import tk.hadeslee.domain.User;
+import tk.hadeslee.repository.UserRepository;
+
+import java.util.List;
 
 /**
  * Project: spring-boot-hello-world
@@ -15,8 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/")
     public @ResponseBody String index(){
         return "Hello hadeslee spring boot!";
     }
+
+    @RequestMapping("/users")
+    public @ResponseBody
+    List<User> getUserList(){
+        return userRepository.findAll();
+    }
+
+
 }
